f3ceac0af7a59e0bbf3c9a52b0d3e9da00c4dbf7
a22joslarfer <a22joslarfer@inspedralbes.cat>
2024-01-31 20:16:11 +0100

    canvis i arreglos del mapa utilitzant mapbox enlloc de leaflet ara

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,13 +1,16 @@
+
+console.log('API_TOKEN_SALT:',);
+console.log('TRANSFER_TOKEN_SALT:',);
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'utJj4t4G/HkhpJTa+iAXnw=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT',  'ZHGorGs1qsdhQCcjdNqXJw=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT' , 'vvOCRPV3UAxmuJuFJFRL5g=='),
     },
   },
   flags: {
