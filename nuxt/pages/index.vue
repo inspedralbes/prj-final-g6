9e3e1a325975521bdf331d96528f4d7f4331d4b4
Polete23 <a20poldonpoz@inspedralbes.cat>
2024-02-01 12:08:04 +0100

    index mejorado

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -10,16 +10,15 @@
             </ul>
         </nav>
 
-        <div id="map" style="height: 720px; width: 100%;"></div>
+        <div id="map" ref="map" style="height: 100%; width: 100%;"></div>
 
-        <div v-if="punto_de_interes_seleccionado" class="info-card">
+        <div v-if="punto_de_interes_seleccionado && pin_seleccionado" class="info-card">
             <div class="card">
                 <div class="card-header">
-                    <h3>{{ punto_de_interes_seleccionado.nombre_discoteca }}</h3>
-
+                    <h3>{{ pin_seleccionado.nombre_discoteca }}</h3>
+                    <div class="card-closer" @click="cerrarPopUp">X</div>
 
                 </div>
-                <div class="card-closer" @click="cerrarPopUp">X</div>
                 <div class="card-body">
                     <!-- <p>{{ punto_de_interes_seleccionado.description }}</p> -->
                     LOREM IPSUM DOLOR, SIT AMET CONSECTETUR ADIPISICING ELIT. VOLUPTAS IUSTO PERSPICIATIS ISTE SINT QUI
@@ -39,27 +38,29 @@
 </template>
 
   
+  
+
 <script>
+import mapboxgl from 'mapbox-gl';
 
 export default {
     head() {
         return {
-            link:
-                [
-                    {
-                        rel: 'preconnect',
-                        href: 'https://fonts.googleapis.com',
-                    },
-                    {
-                        rel: 'preconnect',
-                        href: 'https://fonts.gstatic.com',
-                        crossorigin: true,
-                    },
-                    {
-                        rel: 'stylesheet',
-                        href: 'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Fjalla+One&display=swap',
-                    },
-                ],
+            link: [
+                {
+                    rel: 'stylesheet',
+                    href: 'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.0/mapbox-gl-geocoder.css',
+                },
+                {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200',
+                },
+                {
+                    rel: 'stylesheet',
+                    href: 'https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css'
+                }
+
+            ],
         };
     },
     data() {
@@ -69,146 +70,222 @@ export default {
             arr_puntos_de_interes: [],
             marker: null,
             data: [],
+            puntos_de_interes: [
+                { coordenadas: [2.0947632393357907, 41.35567342431939], nombre_discoteca: 'MALALTS DE FESTA' },
+                { coordenadas: [2.19113223925985, 41.397917303330644], nombre_discoteca: 'RAZZMATAZZ' },
+                { coordenadas: [2.157926368172369, 41.393762946100026], nombre_discoteca: 'TWENTIES BARCELONA' },
+                { coordenadas: [2.169596668171634, 41.37456718887626], nombre_discoteca: 'SALA APOLO' },
+                { coordenadas: [2.188619054677886, 41.39603579270717], nombre_discoteca: 'WOLF BARCELONA' },
+                { coordenadas: [2.109363168171127, 41.36033448432702], nombre_discoteca: 'SALA SALAMANDRA' },
+                { coordenadas: [2.1522306970075755, 41.39565454188381], nombre_discoteca: 'LA BIBLIO BARCELONA' },
+            ],
+            icono_llevar_a_barcelona: null,
         };
     },
     mounted() {
 
-        if (process.client) {
+        this.initMapaDatosMapBox();
 
-            const leafletCSS = document.createElement('link');
-            leafletCSS.rel = 'stylesheet';
-            leafletCSS.href = 'https://unpkg.com/leaflet/dist/leaflet.css';
-            document.head.appendChild(leafletCSS);
+        this.map.on('load', () => {
+
+            this.crear_mostrar_pines_discos();
+
+            this.crear_mostrar_pin_barcelona();
+
+            this.establecer_visibilidad_segun_zoom_pines();
+
+            this.añadir_popup_info_de_las_discos();
 
 
-            const leafletJS = document.createElement('script');
-            leafletJS.src = 'https://unpkg.com/leaflet/dist/leaflet.js';
-            leafletJS.onload = this.initMap;
-            document.body.appendChild(leafletJS);
-        }
+        });
 
 
 
     },
 
     methods: {
-        cerrarPopUp() {
-            this.punto_de_interes_seleccionado = null;
-        },
-        initMap() {
-            this.map = L.map('map', {
-                center: [40, -4],
-                zoom: 6,
-                minZoom: 6,
-                maxZoom: 17,
+        initMapaDatosMapBox() {
+            mapboxgl.accessToken = 'pk.eyJ1IjoiYTIyam9zbGFyZmVyIiwiYSI6ImNsczIwdDY5YTBldncyc21rbmI4cnVjY3oifQ.mWjSoIuuwJmMG0EFCU_gEA';
+
+            this.map = new mapboxgl.Map({
+                container: this.$refs.map,
+                style: 'mapbox://styles/a22joslarfer/cls1zcyoh013v01qy03s63ok6',
+                center: [2.0947632393357907, 39.35567342431939],
+                zoom: 5,
             });
+      
+        },
+        crear_mostrar_pines_discos() {
+            if (this.map.getLayer('points')) {
+                this.map.removeLayer('points');
+            }
 
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '© OpenStreetMap contributors',
-            }).addTo(this.map);
-
-
-            const locations = [
-                { latLng: [41.35567342431939, 2.0947632393357907], nombre_discoteca: 'MALALTS DE FESTA', descripcion: '', },
-                { latLng: [41.397917303330644, 2.19113223925985], nombre_discoteca: 'RAZZMATAZZ' },
-                { latLng: [41.393762946100026, 2.157926368172369], nombre_discoteca: 'TWENTIES BARCELONA' },
-                { latLng: [41.37456718887626, 2.169596668171634], nombre_discoteca: 'SALA APOLO' },
-                { latLng: [41.39603579270717, 2.188619054677886], nombre_discoteca: 'WOLF BARCELONA' },
-                { latLng: [41.36033448432702, 2.109363168171127], nombre_discoteca: 'SALA SALAMANDRA' },
-                { latLng: [41.39565454188381, 2.1522306970075755], nombre_discoteca: 'LA BIBLIO BARCELONA' },
-            ];
-
-            this.map.on('zoomend', () => {
-                this.clearMarkers();
-                this.crearLocalizacionesEnElMapa(locations);
-                if (this.map.getZoom() >= 12) {
-                    llevar_a_barcelona.remove();
-                } else {
-                    llevar_a_barcelona.addTo(this.map);
+            this.map.addSource('points', {
+                'type': 'geojson',
+                'data': {
+                    'type': 'FeatureCollection',
+                    'features': this.puntos_de_interes.map((punto, index) => ({
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': punto.coordenadas,
+                        },
+                        'properties': {
+                            'id': index,
+                            'nombre_discoteca': punto.nombre_discoteca,
+                            'horario': '',
+                            'imagen': '',
+                            'descripcion': '',
+                            'direccion': '',
+                            'telefono': '',
+                            'web': '',
+                        }
+                    }))
                 }
             });
 
-            var barcelonaIcon = L.icon({
-                iconUrl: './mark.png',
 
-                iconSize: [40, 40], // size of the icon
-                iconAnchor: [22, 54], // point of the icon which will correspond to marker's location
 
-                popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+            this.map.addLayer({
+                'id': 'points',
+                'type': 'circle',
+                'source': 'points',
+                'layout': {
+                    'visibility': 'visible'
+                },
+                'paint': {
+                    'circle-radius': 10,
+                    'circle-color': '#dfdfdf',
+                },
+                'minzoom': 10,
+                'maxzoom': 15,
             });
 
 
-            var llevar_a_barcelona = L.marker([41.39565454188381, 2.1522306970075755], { icon: barcelonaIcon }).addTo(this.map);
-            llevar_a_barcelona.on('click', () => {
-                this.map.setView([41.35567342431939, 2.0947632393357907], 12);
-            });
-
-
-            this.crearLocalizacionesEnElMapa(locations);
         },
+        crear_mostrar_pin_barcelona() {
 
-        crearLocalizacionesEnElMapa(locations) {
-            const zoomLevel = this.map.getZoom();
+            this.map.addLayer({
+                'id': 'barcelona-pin',
+                'type': 'circle',
+                'source': {
+                    'type': 'geojson',
+                    'data': {
+                        'type': 'FeatureCollection',
+                        'features': [{
+                            'type': 'Feature',
+                            'geometry': {
+                                'type': 'Point',
+                                'coordinates': [2.0947632393357907, 41.35567342431939],
+                            },
+                            'properties': {
+                                'id': 'barcelona-pin',
+                            }
+                        }]
+                    }
+                },
+                'layout': {
+                    'visibility': 'visible'
+                },
+                'paint': {
+                    'circle-radius': 15,
+                    'circle-color': '#ff0000',
+                },
+            });
+
+            
+
+        },
+        establecer_visibilidad_segun_zoom_pines() {
+
+            const establecer_visibilidad_pines = () => {
+                const currentZoom = this.map.getZoom();
+                if (currentZoom >= 12) {
+                    this.map.setLayoutProperty('points', 'visibility', 'visible');
+                    this.map.setLayoutProperty('barcelona-pin', 'visibility', 'none');
+                } else {
+                    this.map.setLayoutProperty('points', 'visibility', 'none');
+                    this.map.setLayoutProperty('barcelona-pin', 'visibility', 'visible');
+                }
+            };
+
+            establecer_visibilidad_pines();
 
 
-            if (zoomLevel >= 12) {
-                locations.forEach((location) => {
-                    const marker = L.marker(location.latLng, { icon: L.icon({ iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png', iconSize: [25, 41], iconAnchor: [12, 41], popupAnchor: [1, -34], shadowSize: [41, 41] }) }).addTo(this.map);
+            this.map.on('zoom', () => {
+                establecer_visibilidad_pines();
+            });
 
 
-                    marker.bindPopup((location.nombre_discoteca));
+            this.map.on('click', 'barcelona-pin', () => {
 
-                    marker.on('click', () => {
-                        this.punto_de_interes_seleccionado = location;
-                        marker.closePopup(); 
-
-                    });
-
-                    this.marker = marker;
-                    this.arr_puntos_de_interes.push(marker);
+                this.map.flyTo({
+                    center: [2.0947632393357907, 41.35567342431939],
+                    zoom: 12,
                 });
-            }
+
+                this.map.setLayoutProperty('barcelona-pin', 'visibility', 'none');
+            });
+        },
+        añadir_popup_info_de_las_discos() {
+            this.map.on('click', 'points', (e) => {
+                // Get the id of the clicked point
+                this.cerrarPopUp();
+                const id = e.features[0].properties.id;
+                console.log('Clicked point id:', id);
+                console.log('Selected point:', this.puntos_de_interes[id]);
+
+                this.punto_de_interes_seleccionado = true;
+                this.pin_seleccionado = this.puntos_de_interes[id];
+            });
+
+        },
+        cerrarPopUp() {
+            this.punto_de_interes_seleccionado = null;
+            this.pin_seleccionado = null;
         },
         clearMarkers() {
-            this.arr_puntos_de_interes.forEach((marker) => {
-                marker.remove();
-            });
+            this.arr_puntos_de_interes.forEach(marker => marker.remove());
             this.arr_puntos_de_interes = [];
         },
+
     },
 };
+
 </script>
   
-<!-- subir foto, crear disctoeca -->
+
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Antonio:wght@700&display=swap');
 
+:root {
+    /* ESQUEMA DE COLORES 1 */
+    --base: hsl(0, 5%, 16%);
+    --verde: hsl(155, 81%, 46%);
+    --rosa: hsl(303, 97%, 85%);
+    --carne: hsl(14, 45%, 89%);
+    --azul: hsl(226, 64%, 58%);
+    --blanco: hsl(0, 0%, 100%);
+    /* ESQUEMA DE COLORES 2 */
+    --base2: hsl(354, 9%, 5%);
+    --verde2: hsl(124, 9%, 32%);
+    --naranja: hsl(32, 85%, 76%);
+    --carne2: hsl(32, 70%, 89%);
+    --rojo: hsl(0, 84%, 15%);
+    --azul2: hsl(213, 15%, 38%);
+}
 
 .container {
     height: 100vh;
 }
 
-.card-closer {
-    cursor: pointer;
-    font-size: 30px;
-    position: absolute;
-    top: 8px;
-    right: 12px;
-
-    color: #fff;
-
-
-}
-
 footer {
-    width: 100%;
-    height: 100px;
-    background-color: #1c1c1c;
     position: absolute;
     bottom: 0;
-    left: 0;
-    right: 0;
-    margin-bottom: 0;
+    width: 100%;
+    height: 50px;
+    background-color: var(--base);
 
 }
 
@@ -216,24 +293,29 @@ footer {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Fjalla One', sans-serif;
-
+    font-family: 'Antonio', sans-serif;
+    overflow: hidden;
+    overflow-y: hidden;
 }
 
+/* ESTILOS PARA LA BARRA DE NAVEGACION */
+
 .navbar {
-    font-size: 30px;
+    font-size: 3vw;
     width: 100%;
     height: 150px;
-    background-color: #1c1c1c;
+    background-color: var(--base);
     font-size: 30px;
 }
 
 .navbar ul {
     display: flex;
-    justify-content: space-around;
+    justify-content: space-evenly;
     align-items: center;
     list-style: none;
     height: 100%;
+    color: var(--blanco);
+
 }
 
 .navbar ul li {
@@ -241,67 +323,107 @@ footer {
     display: flex;
     justify-content: center;
     align-items: center;
+    font-size: 60px;
 }
 
 .navbar ul li:hover,
 .navbar ul li a:hover {
-    color: #ffffff;
+    color: var(--carne2);
 }
 
 .navbar ul li a {
     text-decoration: none;
-    color: #dfdfdf;
-    font-weight: 600;
+    color: var(----blanco);
 }
 
 
-#Barcelona {
-    color: #dfdfdf;
-    font-weight: 600;
-    cursor: pointer;
-}
-
-#map {
-    height: 100%;
-    width: 100%;
-}
-
+/* ESTILOS PARA LOS PANELES DE INFORMACION */
 .info-card {
     position: absolute;
     top: 155px;
     right: 30px;
     z-index: 1000;
     max-width: 450px;
-    background-color: #ffffff;
-    border: 2px solid #1c1c1c;
+    background-color: var(--base);
     border-radius: 10px;
-    color: #1c1c1c;
-    box-shadow: 0 2px 5px rgb(0, 0, 0);
-
+    color: var(--verde2);
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.87);
+    animation: fade-in 0.5s ease-in-out;
 }
 
+@keyframes fade-in {
+    0% {
+        opacity: 0.33;
+        transform: translateY(40px);
+    }
 
-.card-header>h3 {
-    margin: 0;
-    font-size: 25px;
-    font-weight: 600;
-    color: #fff;
-    text-align: center;
+    100% {
+        opacity: 1;
+        transform: translateY(0);
+    }
+}
+
+.card-closer {
+    cursor: pointer;
+    font-size: 2vw;
+    color: var(--base);
+    transition: color 0.2s ease-in-out;
+}
+
+.card-closer:hover {
+    color: #ffffff;
 }
 
 .card-header {
-    background-color: #007BFF;
-    border-radius: 8px;
-    color: #fff;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    background-color: var(--verde2);
+    border-radius: 5px 5px 0 0;
     padding: 10px;
+    font-size: 2vw;
+    color: var(--base);
 
 }
+
+
+
 
 .card-body {
     text-align: justify;
     padding: 20px;
     line-height: 1.1;
+    font-size: 1.2vw;
 
-    font-weight: bold;
+}
+
+/* ESTILOS PARA MOBIL */
+
+/* Media queries for responsiveness */
+@media only screen and (max-width: 768px) {
+
+    .navbar {
+        font-size: 20px;
+        height: 100px;
+    }
+
+    .navbar ul li {
+        font-size: 30px;
+    }
+
+    .info-card {
+        top: 105px;
+        right: 10px;
+        max-width: 350px;
+    }
+
+    .card-header>h3 {
+        font-size: 30px;
+    }
+
+    .card-body {
+        font-size: 20px;
+    }
+
 }
 </style>
