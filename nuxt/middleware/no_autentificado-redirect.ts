6d9be808f82b7d5931ee7981bc62d96860cef386
a22joslarfer <a22joslarfer@inspedralbes.cat>
2024-02-13 12:22:27 +0100

    delete laravel y nuxt

--- nuxt/middleware/no_autentificado-redirect.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware(async (to, from) => {
-  const usuarioStore = useUsuarioStore();
-
-  // User not logged in
-  if (!usuarioStore.user) {
-    return await navigateTo('/auth/login/');
-  }
-});
