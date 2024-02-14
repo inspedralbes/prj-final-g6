ee54e5b8886af3a7d8b951be1cba1cc064213348
Polete23 <a20poldonpoz@inspedralbes.cat>
2024-02-14 10:37:22 +0100

    canvis per funcionament

--- laravel/backend/app/Http/Middleware/VerifyCsrfToken.php
+++ laravel/backend/app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        ['api/register', 'api/csrf-token']
+        ['api/register', 'api/csrf-token' , 'api/reviews']
     ];
 }
--- laravel/backend/app/Models/reviewModel.php
+++ laravel/backend/app/Models/reviewModel.php
@@ -11,7 +11,7 @@ class reviewModel extends Model
 
     
     protected $fillable = [
-        'user_id',
+        'usuario_id',
         'disco_id',
         'titulo',
         'puntuacion',
