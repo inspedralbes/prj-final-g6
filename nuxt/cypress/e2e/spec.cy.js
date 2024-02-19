a823c281cdea90d10ca88d5a5832381066b0a511
Polete23 <a20poldonpoz@inspedralbes.cat>
2024-02-19 09:48:18 +0100

    testing crear reseña

---
+++ nuxt/cypress/e2e/spec.cy.js
@@ -0,0 +1,23 @@
+describe('Crear Reseñas', () => {
+  it('Permite a los usuarios crear reseñas', () => {
+    cy.visit('http://localhost:3000/CRUD/REVIEWS/Crear-Review/3')
+
+    cy.get('input[type="text"]').type('Título de la reseña')
+    cy.get('textarea').type('Este es un comentario de la reseña.')
+    
+    cy.fixture('micu.jpeg').then(fileContent => {
+      cy.get('input[type="file"]').attachFile({
+        fileContent: fileContent.toString(),
+        fileName: 'micu.jpeg',
+        mimeType: 'image/jpeg'
+      });
+    });
+
+    cy.get('input[type="radio"]').check('5', { force: true });
+
+    cy.get('form').submit()
+
+    cy.url().should('include', '/')
+    cy.log('Reseña enviada correctamente');
+  })
+})
